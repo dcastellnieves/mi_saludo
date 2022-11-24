3c55b5f86f78b8accbad83c549806f059ae6d693
dcastellnieves <119002252+dcastellnieves@users.noreply.github.com>
2022-11-24 19:28:10 +0000

    Update App.java

--- src/main/java/es/ull/app/App.java
+++ src/main/java/es/ull/app/App.java
@@ -4,11 +4,28 @@ package es.ull.app;
  * Hello world!
  *
  */
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+                for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
         System.out.println( "Parece que Rula...!" );
     }
+    
+    double a,c;
+             	    Scanner sc=new Scanner(System.in);	   	 
+	    System.out.println("Enter  Fahrenheit temperature");
+                   a=sc.nextDouble(); 
+	    System.out.println("Celsius temperature is = "+celsius(a));		  	  	     
+	} 
+	static double celsius(double f)
+	{	
+	return  (f-32)*5/9;
+	}
+    
 }
